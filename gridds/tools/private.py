a846c3dc7f65ed413e54e0f88d6ae6df1b2deb57
Ladd <ladd12@seatac52.llnl.gov>
2022-09-12 13:53:44 -0700

    ignore private again on gitignore

--- gridds/tools/private.py
+++ gridds/tools/private.py
@@ -1,9 +1,8 @@
 
 
-
-
-CONNECTION = "xxxx"
-host="xxx"
-database="xx"
-user="xxxx"
-password="xxxx"
+CO_OP_NAMES = ['southside_ec', 'GCEC', 'HolyCross']
+CONNECTION = "postgres://postgres:gmlcnreca2021@crystalcreek:5432/postgres"
+host="crystalcreek"
+database="postgres"
+user="postgres"
+password="gmlcnreca2021"
